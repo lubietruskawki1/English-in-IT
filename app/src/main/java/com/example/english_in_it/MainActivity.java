a49ea1417fe889b1ae8221bca8c016b2fce41755
Agata Zaleska <az429700@students.mimuw.edu.pl>
2022-04-26 16:59:05 +0200

    Połączenie aplikacji z bazą danych, strona główna i menu startowe.

--- app/src/main/java/com/example/english_in_it/MainActivity.java
+++ app/src/main/java/com/example/english_in_it/MainActivity.java
@@ -18,7 +18,7 @@ public class MainActivity extends AppCompatActivity {
         startButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this, StartMenuActivity.class);
+                Intent intent = new Intent(MainActivity.this, StartListActivity.class);
                 startActivity(intent);
             }
         });
--- app/src/main/java/com/example/english_in_it/StartListActivity.java
+++ app/src/main/java/com/example/english_in_it/StartListActivity.java
@@ -1,19 +1,13 @@
 package com.example.english_in_it;
 
-import android.content.Intent;
-import android.graphics.Color;
-import android.view.View;
-import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
-import android.widget.Button;
 import android.widget.ListView;
-import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 import java.util.ArrayList;
 
-public class StartMenuActivity extends AppCompatActivity {
+public class StartListActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,29 +23,11 @@ public class StartMenuActivity extends AppCompatActivity {
         menu_elements.add("Play falling comet");
         menu_elements.add("Set course preferences");
 
-        /*ArrayAdapter<String> menu_adapter = new ArrayAdapter<String>(
-                this, android.R.layout.simple_list_item_1, menu_elements) {
-
-            @Override
-            public View getView(int position, View convert_view, ViewGroup parent) {
-                View view = super.getView(position, convert_view, parent);
-
-                TextView text = findViewById(android.R.id.text1);
-                text.setTextColor(Color.WHITE);
-
-                return view;
-            }
-
-        };*/
 
         ArrayAdapter<String> menu_adapter = new ArrayAdapter<String>(
                 this, android.R.layout.simple_list_item_1, menu_elements);
 
         menuList.setAdapter(menu_adapter);
-
-
-
-
 
 
     }
