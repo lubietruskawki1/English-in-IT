13c848dc07f6ce894d57c185aa74ffc6d3ef1458
Zofia Ogonek <zosia.ogonek@gmail.com>
2022-04-27 21:11:29 +0200

    Łączenie się z bazą danych i wypisywanie słownictwa.

--- app/src/main/java/com/example/english_in_it/BrowseVocabulary.java
+++ app/src/main/java/com/example/english_in_it/BrowseVocabulary.java
@@ -1,21 +1,31 @@
 package com.example.english_in_it;
 
-import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Pair;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import androidx.appcompat.app.AppCompatActivity;
 
-import java.sql.Connection;
 import java.sql.SQLException;
-import java.sql.Statement;
+import java.util.ArrayList;
 
 public class BrowseVocabulary extends AppCompatActivity {
+    private ConnectionHandler connection_handler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_browse_vocabulary);
 
-        ConnectionHandler connection_handler = new ConnectionHandler();
-        connection_handler.HandleConnection();
-        Connection c = connection_handler.getConnection();
+        connection_handler = new ConnectionHandler(BrowseVocabulary.this);
+
+        ArrayList<String> glossary = connection_handler.getGlossary();
+        System.out.println("ROZMIAR " + glossary.size());
+
+        ListView menuList = findViewById(R.id.menuElementsList);
+        ArrayAdapter<String> menu_adapter = new ArrayAdapter<>(
+                BrowseVocabulary.this, android.R.layout.simple_list_item_1, glossary);
+
+        menuList.setAdapter(menu_adapter);
     }
 }
--- app/src/main/java/com/example/english_in_it/ConnectionHandler.java
+++ app/src/main/java/com/example/english_in_it/ConnectionHandler.java
@@ -1,68 +1,72 @@
 package com.example.english_in_it;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
 
-public class ConnectionHandler {
-    public ConnectionHandler() {}
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Pair;
 
-    private Connection connection = null;
+import java.util.ArrayList;
 
-    public void HandleConnection () {
-        try {
-            // create a database connection
-            connection = DriverManager.getConnection("jdbc:sqlite:sample.db");
-            Statement statement = connection.createStatement();
-            statement.setQueryTimeout(30);  // set timeout to 30 sec.
+public class ConnectionHandler extends SQLiteOpenHelper {
+    private static final String DB_NAME = "sample";
+    private static final int DB_VERSION = 1;
 
-            statement.executeUpdate("drop table if exists glossary");
-            statement.executeUpdate("create table glossary (term string, definition string)");
+    public ConnectionHandler(Context context) {
+        super(context, DB_NAME, null, DB_VERSION);
+    }
 
-            // przykładowe 10 pierwszych z https://en.wikipedia.org/wiki/Glossary_of_computer_science
-            statement.executeUpdate("insert into glossary values('abstract data type', 'A mathematical model for data types in which a data type is defined by its behavior (semantics) from the point of view of a user of the data, specifically in terms of possible values, possible operations on data of this type, and the behavior of these operations.')");
-            statement.executeUpdate("insert into glossary values('abstract method', 'One with only a signature and no implementation body.')");
-            statement.executeUpdate("insert into glossary values('abstraction', 'In software engineering and computer science, the process of removing physical, spatial, or temporal details or attributes in the study of objects or systems in order to more closely attend to other details of interest; it is also very similar in nature to the process of generalization.')");
-            statement.executeUpdate("insert into glossary values('agent architecture', 'A blueprint for software agents and intelligent control systems depicting the arrangement of components.')");
-            statement.executeUpdate("insert into glossary values('agent-based model', 'A class of computational models for simulating the actions and interactions of autonomous agents (both individual or collective entities such as organizations or groups) with a view to assessing their effects on the system as a whole.')");
-            statement.executeUpdate("insert into glossary values('aggregate function', 'In database management, a function in which the values of multiple rows are grouped together to form a single value of more significant meaning or measurement, such as a sum, count, or max.')");
-            statement.executeUpdate("insert into glossary values('agile software development', 'An approach to software development under which requirements and solutions evolve through the collaborative effort of self-organizing and cross-functional teams and their customer(s)/end user(s).')");
-            statement.executeUpdate("insert into glossary values('algorithm', 'An unambiguous specification of how to solve a class of problems.')");
-            statement.executeUpdate("insert into glossary values('algorithm design', 'A method or mathematical process for problem-solving and for engineering algorithms.')");
-            statement.executeUpdate("insert into glossary values('algorithmic efficiency', 'A property of an algorithm which relates to the number of computational resources used by the algorithm.')");
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL("drop table if exists glossary");
+        String query = "create table glossary (term text, definition text)";
+        db.execSQL(query);
+        fillDatabase();
+    }
 
-            ResultSet rs = statement.executeQuery("select * from glossary");
+    public void fillDatabase() {
+        SQLiteDatabase db = this.getWritableDatabase();
 
-            int i = 1;
-            while (rs.next()) {
-                // read the result set
-                System.out.print(i + ". term = " + rs.getString("term"));
-                System.out.println("definition = " + rs.getString("definition") + "\n");
-                i++;
-            }
-        } catch(SQLException e) {
-            // if the error message is "out of memory",
-            // it probably means no database file is found
-            System.err.println(e.getMessage());
-        } finally {
-            try {
-                if (connection != null) {
-                    connection.close();
-                }
-            } catch(SQLException e) {
-                // connection close failed.
-                System.err.println(e.getMessage());
-            }
+        // tak jest ładniej ale nie chce mi sie xd
+        ContentValues values = new ContentValues();
+        values.put("term", "abstract data type");
+        values.put("definition", "A mathematical model for data types in which a data type is defined by its behavior (semantics) from the point of view of a user of the data, specifically in terms of possible values, possible operations on data of this type, and the behavior of these operations.");
+        db.insert("glossary", null, values);
+
+        //db.execSQL("insert into glossary values('abstract data type', 'A mathematical model for data types in which a data type is defined by its behavior (semantics) from the point of view of a user of the data, specifically in terms of possible values, possible operations on data of this type, and the behavior of these operations.')");
+        db.execSQL("insert into glossary values('abstract method', 'One with only a signature and no implementation body.')");
+        db.execSQL("insert into glossary values('abstraction', 'In software engineering and computer science, the process of removing physical, spatial, or temporal details or attributes in the study of objects or systems in order to more closely attend to other details of interest; it is also very similar in nature to the process of generalization.')");
+        db.execSQL("insert into glossary values('agent architecture', 'A blueprint for software agents and intelligent control systems depicting the arrangement of components.')");
+        db.execSQL("insert into glossary values('agent-based model', 'A class of computational models for simulating the actions and interactions of autonomous agents (both individual or collective entities such as organizations or groups) with a view to assessing their effects on the system as a whole.')");
+        db.execSQL("insert into glossary values('aggregate function', 'In database management, a function in which the values of multiple rows are grouped together to form a single value of more significant meaning or measurement, such as a sum, count, or max.')");
+        db.execSQL("insert into glossary values('agile software development', 'An approach to software development under which requirements and solutions evolve through the collaborative effort of self-organizing and cross-functional teams and their customer(s)/end user(s).')");
+        db.execSQL("insert into glossary values('algorithm', 'An unambiguous specification of how to solve a class of problems.')");
+        db.execSQL("insert into glossary values('algorithm design', 'A method or mathematical process for problem-solving and for engineering algorithms.')");
+        db.execSQL("insert into glossary values('algorithmic efficiency', 'A property of an algorithm which relates to the number of computational resources used by the algorithm.')");
+
+        db.close();
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("drop table if exists glossary");
+        onCreate(db);
+    }
+
+    public ArrayList<String> getGlossary() {
+        ArrayList<String> glossary = new ArrayList<>();
+        String selectQuery = "select * from glossary";
+
+        SQLiteDatabase db = this.getReadableDatabase();
+        Cursor cursor = db.rawQuery(selectQuery, null);
+
+        if (cursor.moveToFirst()) {
+            do {
+                glossary.add(cursor.getString(0) + ": " + cursor.getString(1));
+            } while (cursor.moveToNext());
         }
-    }
 
-    public Connection getConnection() {
-        return connection;
-    }
-
-    public static void main() {
-        ConnectionHandler ch = new ConnectionHandler();
-        ch.HandleConnection();
+        return glossary;
     }
 }
