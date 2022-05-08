df4f41493b0d85564a4eb71ea3cffab31f3610fe
Forsycja <olakurc@onet.pl>
2022-05-08 22:56:06 +0200

    ładny RepeatListener + baardzo prymitywne TypingWordsExercise

--- app/src/main/java/com/example/english_in_it/TypingWordsExercise.java
+++ app/src/main/java/com/example/english_in_it/TypingWordsExercise.java
@@ -1,12 +1,19 @@
 package com.example.english_in_it;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 
 import java.util.ArrayList;
+import java.util.Date;
+import java.util.Iterator;
+
 
 public class TypingWordsExercise extends AppCompatActivity {
 
@@ -14,17 +21,39 @@ public class TypingWordsExercise extends AppCompatActivity {
 
     private ConnectionHandler connection_handler;
 
+    private int mInterval = 5000; // 5 seconds by default, can be changed later
+    private Handler mHandler;
 
+    public TextView meaning;
+    public Button check_button;
+    public EditText word;
+    private Iterator<Word> iter = words.iterator();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Word word1 = new Word("a","b",0,0,new Date());
+        Word word2 = new Word("c","d",0,0,new Date());
+        words.add(word1);
+        words.add(word2);
+        Iterator<Word> iter = words.iterator();
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_typing_words);
 
         connection_handler = new ConnectionHandler(TypingWordsExercise.this);
 
-        TextView meaning = findViewById(R.id.meaning);
-        Button check_button = findViewById(R.id.check_button);
-        EditText word = findViewById(R.id.word);
+        meaning = findViewById(R.id.meaning);
+        check_button = findViewById(R.id.check_button);
+        word = findViewById(R.id.word);
+
+        check_button.setOnTouchListener(new RepeatListener(400, 100, new View.OnClickListener() {
+            @SuppressLint("ClickableViewAccessibility")
+            @Override
+            public void onClick(View view) {
+                meaning.setText(iter.next().meaning);
+                //Toast.makeText(TypingWordsExercise.this, "Yup.", Toast.LENGTH_SHORT).show();
+            }
+        }));
     }
+
 
 }
