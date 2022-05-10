0ed2641b284bf0f1d0bb83a69ac2ff94382945f7
Forsycja <olakurc@onet.pl>
2022-05-10 11:31:33 +0200

    działające TypingWordsExercise z przełączaniem

--- app/src/main/java/com/example/english_in_it/TypingWordsExercise.java
+++ app/src/main/java/com/example/english_in_it/TypingWordsExercise.java
@@ -2,6 +2,7 @@ package com.example.english_in_it;
 
 import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.View;
@@ -28,8 +29,12 @@ public class TypingWordsExercise extends AppCompatActivity {
     public TextView meaning;
     public Button check_button;
     public EditText word;
+    public TextView good_bad;
+    public TextView correct_answer;
     private Iterator<Word> iter = words.iterator();
     private Word current_word;
+    private int counter_for_clicks;
+    private boolean correct_current_answer;
     @SuppressLint("ClickableViewAccessibility")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -47,30 +52,55 @@ public class TypingWordsExercise extends AppCompatActivity {
         meaning = findViewById(R.id.meaning);
         check_button = findViewById(R.id.check_button);
         word = findViewById(R.id.word);
+        good_bad = findViewById(R.id.good_bad);
+        correct_answer = findViewById(R.id.correct_answer);
 
         current_word = iter.next();
         meaning.setText(current_word.meaning);
+        counter_for_clicks = 0;
+        correct_answer.setText("");
+        good_bad.setText("");
         check_button.setOnTouchListener(new RepeatListener(400, 100, new View.OnClickListener() {
-
             @SuppressLint("ClickableViewAccessibility")
             @Override
             public void onClick(View view) {
-                String entered_word = word.getText().toString();
-                if(entered_word.equals(current_word.word)) {
-                    Toast.makeText(TypingWordsExercise.this, "CORRECT!", Toast.LENGTH_SHORT).show();
+                if(counter_for_clicks%2 == 0) {//pojawia się nowe słówko, użytkownik ma możliwośćc wpisania tłumaczenia
+                    //check_button.setText("CHECK");
+                    correct_answer.setText("");
+                    good_bad.setText("");
+                    String entered_word = word.getText().toString();
+                    if (entered_word.equals(current_word.word)) {
+                        //Toast.makeText(TypingWordsExercise.this, "CORRECT!", Toast.LENGTH_SHORT).show();
+                        //correct_current_answer = true;
+                        good_bad.setText("CORRECT!");
+                        //good_bad.setTextColor(Color.parseColor("0xff00ff00"));
+                    } else {
+                        //Toast.makeText(TypingWordsExercise.this, "Incorrect, correct answer is: " + current_word.word, Toast.LENGTH_SHORT).show();
+                        //correct_current_answer = false;
+                        good_bad.setText("INCORRECT");
+                        correct_answer.setText("correct answer: " + current_word.word);
+                        //good_bad.setTextColor(Color.parseColor("0xffff0000"));
+                    }
+                    check_button.setText("CONTINUE");
+                    counter_for_clicks++;
                 }
-                else {
-                    Toast.makeText(TypingWordsExercise.this, "Incorrect, correct answer is: " + current_word.word, Toast.LENGTH_SHORT).show();
-                }
-                if(!iter.hasNext()) {
-                    Toast.makeText(TypingWordsExercise.this, "FINISHED", Toast.LENGTH_SHORT).show();
-                    Intent intent = new Intent(TypingWordsExercise.this, StartListActivity.class);
-                    startActivity(intent);
-                    return;
-                }
-                current_word = iter.next();
-                meaning.setText(current_word.meaning);
+                else {//pojawia się nowe słówko
+                    //good_bad.setTextColor(Color.parseColor("0xff000000"));
+                    correct_answer.setText("");
+                    good_bad.setText("");
+                    check_button.setText("CHECK");
+                    if (!iter.hasNext()) {
+                        //Toast.makeText(TypingWordsExercise.this, "FINISHED", Toast.LENGTH_SHORT).show();
+                        Intent intent = new Intent(TypingWordsExercise.this, StartListActivity.class);
+                        startActivity(intent);
+                        return;
+                    }
+                    current_word = iter.next();
+                    meaning.setText(current_word.meaning);
+                    word.setText("");
 
+                    counter_for_clicks++;
+                }
             }
         }));
     }
