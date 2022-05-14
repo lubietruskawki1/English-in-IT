09b687d0d6282baed757f780b34fb8c879437dc4
Zofia Ogonek <zosia.ogonek@gmail.com>
2022-05-14 13:22:36 +0200

    Inne słówka w bazie do łatwiejszego testowania

--- app/src/main/java/com/example/english_in_it/ConnectionHandler.java
+++ app/src/main/java/com/example/english_in_it/ConnectionHandler.java
@@ -11,7 +11,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 
 public class ConnectionHandler extends SQLiteOpenHelper {
-    private static final String DB_NAME = "sample";
+    private static final String DB_NAME = "glossary";
     private static final int DB_VERSION = 1;
 
     public ConnectionHandler(Context context) {
@@ -20,33 +20,29 @@ public class ConnectionHandler extends SQLiteOpenHelper {
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-        db.execSQL("drop table if exists glossary");
-        String query = "create table glossary (term text, definition text)";
+        db.execSQL("drop table if exists glossary"); // to będzie do wykasowania gdy już będzie gotowa baza
+        String query = "create table if not exists glossary (term text unique, definition text unique)";
         db.execSQL(query);
         fillDatabase(db);
     }
 
     public void fillDatabase(SQLiteDatabase db) {
-        //SQLiteDatabase db = this.getWritableDatabase();
-
         // tak jest ładniej ale nie chce mi sie xd
         ContentValues values = new ContentValues();
         values.put("term", "abstract data type");
-        values.put("definition", "A mathematical model for data types in which a data type is defined by its behavior (semantics) from the point of view of a user of the data, specifically in terms of possible values, possible operations on data of this type, and the behavior of these operations.");
+        values.put("definition", "abstract data type definition");
         db.insert("glossary", null, values);
 
         //db.execSQL("insert into glossary values('abstract data type', 'A mathematical model for data types in which a data type is defined by its behavior (semantics) from the point of view of a user of the data, specifically in terms of possible values, possible operations on data of this type, and the behavior of these operations.')");
-        db.execSQL("insert into glossary values('abstract method', 'One with only a signature and no implementation body.')");
-        db.execSQL("insert into glossary values('abstraction', 'In software engineering and computer science, the process of removing physical, spatial, or temporal details or attributes in the study of objects or systems in order to more closely attend to other details of interest; it is also very similar in nature to the process of generalization.')");
-        db.execSQL("insert into glossary values('agent architecture', 'A blueprint for software agents and intelligent control systems depicting the arrangement of components.')");
-        db.execSQL("insert into glossary values('agent-based model', 'A class of computational models for simulating the actions and interactions of autonomous agents (both individual or collective entities such as organizations or groups) with a view to assessing their effects on the system as a whole.')");
-        db.execSQL("insert into glossary values('aggregate function', 'In database management, a function in which the values of multiple rows are grouped together to form a single value of more significant meaning or measurement, such as a sum, count, or max.')");
-        db.execSQL("insert into glossary values('agile software development', 'An approach to software development under which requirements and solutions evolve through the collaborative effort of self-organizing and cross-functional teams and their customer(s)/end user(s).')");
-        db.execSQL("insert into glossary values('algorithm', 'An unambiguous specification of how to solve a class of problems.')");
-        db.execSQL("insert into glossary values('algorithm design', 'A method or mathematical process for problem-solving and for engineering algorithms.')");
-        db.execSQL("insert into glossary values('algorithmic efficiency', 'A property of an algorithm which relates to the number of computational resources used by the algorithm.')");
-
-        //db.close();
+        db.execSQL("insert into glossary values('abstract method', 'abstract method definition')");
+        db.execSQL("insert into glossary values('abstraction', 'abstraction definition')");
+        db.execSQL("insert into glossary values('agent architecture', 'agent architecture definition')");
+        db.execSQL("insert into glossary values('agent-based model', 'agent-based model definition')");
+        db.execSQL("insert into glossary values('aggregate function', 'aggregate function definition')");
+        db.execSQL("insert into glossary values('agile software development', 'agile software development definition')");
+        db.execSQL("insert into glossary values('algorithm', 'algorithm definition')");
+        db.execSQL("insert into glossary values('algorithm design', 'algorithm design definition')");
+        db.execSQL("insert into glossary values('algorithmic efficiency', 'algorithmic efficiency definition')");
     }
 
     @Override
